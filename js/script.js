658689e1d4a88d06a7219a534462753dc30a80fc
Kiana Padilla <babygirlkiana211@gmail.com>
2018-12-03 21:31:12 +0000

    lol

--- js/script.js
+++ js/script.js
@@ -4,10 +4,32 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-var userChoice = ""
-var computerChoice = ""
-var winner = ""
-var randomNumber = 0
+
+
+
+$("#shoot").click(function(){
+    var randomNumber = Math.floor((Math.random() * 3));
+    var choices = ["rock","paper","scissors"];
+    var computerChoice= choices[randomNumber];
+    var userChoice = $("#input").val();
+    
+   $("#userChoice").text(userChoice);
+   $("#computerChoice").text(computerChoice)
+   
+  if((userChoice === "rock" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "paper")){
+     $("#user").text("Draw!")
+     $("#comp").text("Draw!")
+    }
+    else if ((userChoice === "rock" && computerChoice === "paper") || (userChoice === "scissors" && computerChoice === "rock") || (userChoice === "paper" && computerChoice === "scissors")){
+     $("#user").text("You Lost!")
+     $("#comp").text("Computer Wins!")
+    }
+    else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "scissors" && computerChoice === "paper") || (userChoice === "paper" && computerChoice === "rock")){
+     $("#user").text("You Win!")
+     $("#comp").text("Computer Loses!")
+    }
+});
+
 
 // DOCUMENT READY FUNCTION BELOW
 
